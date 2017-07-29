c900b2464b9590af7e9231c2dd8971ae6232ab54
Parsons-David <david.parsons@rutgers.edu>
2017-07-29 16:20:16 -0400

    Start Arguement Parsing

--- Makefile
+++ Makefile
@@ -1,9 +1,18 @@
 COMPILER = g++
 
-all: y86emul
+all: y86emul test
 
 y86emul: y86emul.cpp y86emul.h
 		$(COMPILER) y86emul.cpp y86emul.h -o emul
+
+test:
+	@echo "------------------------------------------"
+	./emul -h
+	@echo "------------------------------------------"
+	./emul --help
+	@echo "------------------------------------------"
+	./emul prog1.y86
+	@echo "------------------------------------------"
 
 clean:
 		rm -f *.o
--- y86emul.cpp
+++ y86emul.cpp
@@ -1,5 +1,13 @@
 # include "y86emul.h"
 
-int main(){
-  std::cout << "Hello World!" << std::endl;
+int main(int argc, char** argv){
+  if(argc > 1){
+    std::string arg1 (argv[1]);
+    if(arg1.compare("--help") == 0 || arg1.compare("-h") == 0){
+      std::cout << "Arguemnet Instructions:" << '\n';
+      std::cout << '\t' << "--help, -h, instructions" << '\n';
+      std::cout << '\t' << "<.y86 file> Y86 source file." << '\n';
+    }
+  }
+  std::cout << "Goodbye!" << '\n';
 }
