2e1ccc2f75cbc35b20defee7d4df7f77af0e2722
Joseph A. Boyle <joe@charredsoftware.com>
2017-08-07 02:16:02 -0400

    spelling
    :)

--- y86emul.cpp
+++ y86emul.cpp
@@ -4,7 +4,7 @@ int main(int argc, char** argv){
   if(argc > 1){
     std::string arg1 (argv[1]);
     if(arg1.compare("--help") == 0 || arg1.compare("-h") == 0){
-      std::cout << "Arguemnet Instructions:" << '\n';
+      std::cout << "Argument Instructions:" << '\n';
       std::cout << '\t' << "--help, -h, instructions" << '\n';
       std::cout << '\t' << "<.y86 file> Y86 source file." << '\n';
     }
